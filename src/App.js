de0d30ba3982a54bf5678d338c1e659c19ee3842
Ezequiel Urrizaga <ezequielurrizaga@smartadtags.com>
2018-07-30 20:53:37 -0300

    x

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      la puta madre
-        
+      <p> Progress bar</p>
         <ProgressBar percentage={65}/>
       </div>
     );
